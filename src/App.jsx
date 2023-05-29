b431be5d59073d57f0411b6f9b7f66f5539e87b6
Michael Peter <michaelhpet@gmail.com>
2023-05-29 20:30:30 +0100

    feat: style app

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
+  state = {
+    fullName: "Michael Peter",
+    bio: "otaku",
+    imgSrc: "/logo512.png",
+    profession: "Sofware Engineer",
+  };
+
   render() {
     return (
       <main className="app">
-        <h1>Hello World</h1>
+        <article>
+          <img src={this.state.imgSrc} alt="Photo" />
+          <h1>{this.state.fullName}</h1>
+          <p>{this.state.bio}</p>
+          <p>{this.state.profession}</p>
+        </article>
       </main>
     );
   }
