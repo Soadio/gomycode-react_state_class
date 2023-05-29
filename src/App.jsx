33f217e4055eac5aa45fdbb2b1a3cf159889ab7e
Michael Peter <michaelhpet@gmail.com>
2023-05-29 20:23:35 +0100

    feat: convert App to class component

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="app">
+        <h1>Hello World</h1>
+      </main>
+    );
+  }
+}
+
+export default App;
